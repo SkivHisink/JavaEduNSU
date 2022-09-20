49a5db72d843f2d9c74ab445bd121a7951e864d3
khorunzhenko <khorunzhenko.as@petrogm.com>
2022-09-20 09:31:04 +0700

    fixes for prof

--- HomeTask1/src/main/java/org/example/Main.java
+++ HomeTask1/src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static final double epsilon = 1e-8;
-
     public static void taskFormula() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter R:");
@@ -13,7 +11,7 @@ public class Main {
         while (true) {
             System.out.print("Enter r:");
             r = scanner.nextDouble();
-            if (Math.abs(R - r) > epsilon) {
+            if (R != r) {
                 break;
             }
             System.out.println("r must be not equal R. Please try again.");
@@ -23,13 +21,24 @@ public class Main {
     }
 
     public static void taskClassSuffering() {
-        var catTest = new Cat("Мурка", 5, 5.5, false);
-        var flatTest = new Flat("пентхаус", 3, 80, false);
+        Scanner scanner = new Scanner(System.in); // создаём сканер для чтения с консоли
+        String catName = scanner.next(); // создаём переменную типа String и считываем значение с консоли
+        int age = scanner.nextInt(); // создаём переменную типа int и -||-
+        double weight = scanner.nextDouble(); // создаём переменную типа double -||-
+        boolean isMan = scanner.nextBoolean(); // создаём переменную типа bool -||-
+        var catTest = new Cat(catName, age, weight, isMan); // создаём объект класса Cat  и передаём прочитанные поля
+        // Делаем всё аналогично
+        String type = scanner.next();
+        int numberOfRooms = scanner.nextInt();
+        double square = scanner.nextDouble();
+        boolean isSold = scanner.nextBoolean();
+        var flatTest = new Flat(type, numberOfRooms, square, isSold);
         catTest.getInfo();
         flatTest.getInfo();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         taskFormula();
         taskClassSuffering();
     }
