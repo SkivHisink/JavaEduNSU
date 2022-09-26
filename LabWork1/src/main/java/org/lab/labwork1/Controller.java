074bd782969696bf8c13e23ee4bc805e2ec03842
khorunzhenko <khorunzhenko.as@petrogm.com>
2022-09-26 17:35:10 +0700

    Added some features for interface

--- LabWork1/src/main/java/org/lab/labwork1/Controller.java
+++ LabWork1/src/main/java/org/lab/labwork1/Controller.java
@@ -1,6 +1,9 @@
 package org.lab.labwork1;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
@@ -11,19 +14,36 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.*;
 
-public class Controller {
+public class Controller
+{
     @FXML
     private Label welcomeText;
-
     @FXML
     private TableView resultTable;
     @FXML
+    private ComboBox calculationType;
+    private String infoBegin = "INFO:";
+    @FXML
+    private Label infoText; // like a log for user
+    private List<String> val;
+    private ObservableList<String> observableList;
+    @FXML
     protected void onHelloButtonClick()
     {
+        //test button
         welcomeText.setText("Welcome to JavaFX Application!");
+        infoText.setText(infoBegin + "Test button is work successfully.");
     }
-
+    public Controller()
+    {
+        //Generating payment types
+        val = new ArrayList<String>();
+        val.add("Annuity payment");
+        val.add("Different payment");
+        observableList = FXCollections.observableList(val);
+    }
     @FXML
     protected void onOpenExcelButtonClick() throws IOException {
         FileChooser chooser = new FileChooser();
@@ -36,9 +56,14 @@ public class Controller {
                 "Any files (*.*)",
                 "*.*");
         chooser.getExtensionFilters().add(extFilter);
+        calculationType.setItems(observableList);
+        calculationType.setValue(val.get(0));
         File file = chooser.showOpenDialog(new Stage());
-        FileInputStream fileInputStream =new FileInputStream(file.getPath());
-        XSSFWorkbook hssfWorkbook = new XSSFWorkbook(fileInputStream);
+        if (file != null)
+        {
+            FileInputStream fileInputStream = new FileInputStream(file.getPath());
+            XSSFWorkbook hssfWorkbook = new XSSFWorkbook(fileInputStream);
+        }
     }
 
 }
